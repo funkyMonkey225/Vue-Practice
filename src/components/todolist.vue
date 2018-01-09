441fe7cd2571308de2295ecd0e056d922352cb63
Mac User <sabbey37@gmail.com>
2018-01-08 23:12:31 -0500

    Edits todolist

---
+++ src/components/todolist.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todolist">
+    <ol>
+    <li v-for="todo in todos">
+      {{ todo.text }}
+    </li>
+  </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todolist',
+  data () {
+    return {
+      todos: [
+        { text: 'Buy Peaches' },
+        { text: 'Eat Oranges' },
+        { text: 'Dance' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
