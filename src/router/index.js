58808e4426524cf8e390d814979a0a3366e71ea3
Mac User <sabbey37@gmail.com>
2018-01-08 23:24:54 -0500

    Adds funtimes component

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import todolist from '@/components/todolist'
+import funtimes from '@/components/funtimes'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/funtimes',
+      name: 'funtimes',
+      component: funtimes
+    },
     {
       path: '/list',
       name: 'todolist',
